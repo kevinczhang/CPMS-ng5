84249c822ecda8ab95821d3847d1cc52c9c36b14
Josh <chaozhang78@icloud.com>
2018-04-10 20:42:06 -0400

    Fixed a bug

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Install express server
 const express = require('express');
 const app = express();
+var router = express.Router();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -11,4 +12,4 @@ router.get('*', function (req, res, next) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
