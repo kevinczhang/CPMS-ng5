ed66a27497e7f8331d41385349fcef269f968cb0
Josh <chaozhang78@icloud.com>
2018-07-10 23:13:48 -0400

    changed baseUrl to heroku service.

--- src/app/shared/app-constants.ts
+++ src/app/shared/app-constants.ts
@@ -61,6 +61,6 @@ export class AppConstants {
 
     readonly adminDisplayedColumns = ['Source', 'Title', 'Number', 'Difficulty', 'Action'];
     readonly userDisplayedColumns = ['Source', 'Title', 'Number', 'Difficulty'];
-    readonly baseUrl = 'http://localhost:5000/api';
-    //readonly baseUrl = 'https://cpms-java.herokuapp.com/cpms-springboot';
+    //readonly baseUrl = 'http://localhost:5000/api';
+    readonly baseUrl = 'https://cpms-java.herokuapp.com/api';
 }
